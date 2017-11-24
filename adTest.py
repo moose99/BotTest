8a33ca5df0a11aa15c39215b695882766e1fdb50
Mustafa Thamer <mus@thamers.com>
2017-11-24 08:45:14 -0500

    got data-url and securekey

--- adTest.py
+++ adTest.py
@@ -87,5 +87,63 @@ CheckResponse(response, "\nADDTOCART - POST")
 # CHECKOUT
 #
 
+#
+# Delivery Start - GET
+#    
+#===============================================================================
+# endpoint = 'https://www.adidas.com/us/delivery-start'
+# headers['Referer'] = 'https://www.adidas.com/us/nmd_r1-primeknit-shoes/BZ0223.html'
+# response = session.get(endpoint, headers=headers)
+# CheckResponse(response, '\nCHECKOUT - DELIVERY START')
+#===============================================================================
+
+#
+# GET DATA URL
+#
+endpoint = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show'
+#headers['Referer'] = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show'
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Cache-Control' : 'no-cache',
+    'Connection' : 'keep-alive',
+    'Host' : 'www.adidas.com',
+    'Pragma' : 'no-cache',
+#    'Referer' : 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/52.0.2743.116 Safari/537.36',
+#    'DNT': '1',
+}
+
+response = session.get(endpoint, headers=headers)
+CheckResponse(response, '\nCHECKOUT - CART-SHOW')
+soup = bs4.BeautifulSoup(response.text, 'html.parser')        
+endpoint = soup.find('div', {'class': 'cart_wrapper rbk_shadow_angle rbk_wrapper_checkout summary_wrapper'})['data-url']
+print("data-url:" + endpoint)
+
+#
+# POST to DATA URL
+#
+headers['Referer'] = endpoint
+headers['Content-Type'] = 'application/x-www-form-urlencoded' 
+payload = {
+    'dwfrm_cart_checkoutCart' : 'Checkout'
+    }
+response = session.post(endpoint, headers=headers, data=payload)
+CheckResponse(response, '\nCHECKOUT - POST DATA_URL')
+
+#
+# CHECKOUT, GET, SHIPPING_SHOW
+#    
+endpoint = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show'
+headers['Content-Type'] = None
+response = session.get(endpoint, headers=headers)
+CheckResponse(response, '\nCHECKOUT - CART-SHOW')
+soup = bs4.BeautifulSoup(response.text, 'html.parser')        
+secureKey = soup.find('input', {'name': 'dwfrm_shipping_securekey'})['value']
+print('Securekey:' + secureKey)
+
 
 print('\nDONE')
