711759dd01b9557353229ec516f51a7058d42932
Mustafa Thamer <mus@thamers.com>
2017-11-24 08:55:42 -0500

    cleanup

--- adTest.py
+++ adTest.py
@@ -58,7 +58,7 @@ def CheckResponse(response, label):
 session = requests.Session()    # START SESSION
 
 #
-# Get ADD TO CART
+# -------------- Get, ADD TO CART ---------------------
 #
 endpoint = 'https://www.adidas.com/api/cart_items?sitePath=us'
 response = session.get(endpoint, headers=headers)
@@ -84,39 +84,13 @@ response = session.post(endpoint, json=payload, headers=headers)
 CheckResponse(response, "\nADDTOCART - POST")
 
 #
-# CHECKOUT
+# ------------------- CHECKOUT ---------------------
 #
-
-#
-# Delivery Start - GET
-#    
-#===============================================================================
-# endpoint = 'https://www.adidas.com/us/delivery-start'
-# headers['Referer'] = 'https://www.adidas.com/us/nmd_r1-primeknit-shoes/BZ0223.html'
-# response = session.get(endpoint, headers=headers)
-# CheckResponse(response, '\nCHECKOUT - DELIVERY START')
-#===============================================================================
 
 #
 # GET DATA URL
 #
 endpoint = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Cart-Show'
-#headers['Referer'] = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show'
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Cache-Control' : 'no-cache',
-    'Connection' : 'keep-alive',
-    'Host' : 'www.adidas.com',
-    'Pragma' : 'no-cache',
-#    'Referer' : 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/52.0.2743.116 Safari/537.36',
-#    'DNT': '1',
-}
-
 response = session.get(endpoint, headers=headers)
 CheckResponse(response, '\nCHECKOUT - CART-SHOW')
 soup = bs4.BeautifulSoup(response.text, 'html.parser')        
@@ -127,7 +101,7 @@ print("data-url:" + endpoint)
 # POST to DATA URL
 #
 headers['Referer'] = endpoint
-headers['Content-Type'] = 'application/x-www-form-urlencoded' 
+headers['Content-Type'] = 'application/x-www-form-urlencoded'  # NOT JSON, use data param not json
 payload = {
     'dwfrm_cart_checkoutCart' : 'Checkout'
     }
@@ -138,9 +112,8 @@ CheckResponse(response, '\nCHECKOUT - POST DATA_URL')
 # CHECKOUT, GET, SHIPPING_SHOW
 #    
 endpoint = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show'
-headers['Content-Type'] = None
 response = session.get(endpoint, headers=headers)
-CheckResponse(response, '\nCHECKOUT - CART-SHOW')
+CheckResponse(response, '\nCHECKOUT - SHIPPING-SHOW')
 soup = bs4.BeautifulSoup(response.text, 'html.parser')        
 secureKey = soup.find('input', {'name': 'dwfrm_shipping_securekey'})['value']
 print('Securekey:' + secureKey)
